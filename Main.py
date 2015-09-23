6b459fa6f8fd10dcdc0eb0e8b7aa7482a3d6d6c5
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-09-22 18:20:11 -0600

    Integrated all parts for task 1

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
+#The 'as' keyword allows using 'info' instead of 'Text' to call 'Text' methods
 #Import the file with text information
 import Text as info
 
 #Import the file with the grid
 import BlankGrid as grid
+
+#Import the file for player moves
+import Move as move
 
 def display_menu():
     #Display a menu
@@ -27,20 +31,23 @@ def main():
     #Evaluate input
     while user_option is not 3:
         if user_option is 1:
-			#Start the game
+			#Print game board
             grid.printblankgrid()
+
+            #Get and print users move
+            print(move.get_move())
         elif user_option is 2:
 			#Call the show rules menu from the Text module
             info.show_rules()
-            
+
             #Show the menu
             display_menu()
         else:
             print("Invalid option, try again!\n")
-            
+
             #Show the menu
             display_menu()
-            
+
         #Ask for input again
         user_option = int(input("Enter your choice: "))
 
