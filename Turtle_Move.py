1e65c9aadee0700b1b0af00e9cc914d14a520bd9
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-09-28 12:19:12 -0600

    Updated to use constants

--- Turtle_Move.py
+++ Turtle_Move.py
@@ -1,4 +1,5 @@
 import string
+import Constants
 
 def get_stripped_line(location):
 	"""
@@ -6,7 +7,7 @@ def get_stripped_line(location):
 	Unneeded characters include blank space and punctuation.
 	Returns a string on only numbers and letters, ex: A8
 	"""
-	
+
 	#Get all characters that can't be used
 	not_needed_chars = string.punctuation + string.whitespace + " "
 
@@ -25,7 +26,7 @@ def get_column_and_row(location):
 	Takes a string in the form letterNumber, ex: B6 and separates number from letter.
 	Returns an array of [letter, number]; ex: ['A', 2]
 	"""
-	
+
 	#Get the stripped line ex: A8
 	column_and_row = get_stripped_line(location)
 
@@ -46,32 +47,29 @@ def check_validity(column, row):
 	"""
 	#columns = {"A" : 1, "B" : 2, "C" :  3, "D" : 4, "E" : 5, "F" : 6, "G" :  7, "H" : 8}
 	#return (columns[column] >= 1 and columns[column] <= 8) and (row >= 1 and row <= 8)
-		
-	#Valid columns
-	columns = "ABCDEFGH"
-	
+
 	#Change entered column to uppercase
 	column = column.upper()
-	
+
 	#Check if column is in the valid columns and is in the rows
-	return (column in columns) and (row >= 1 and row <= 8)
+	return (column in Constants.COLUMN_LETTERS) and (row in Constants.ROW_NUMBERS)
 
 def place_piece(column, row):
 	"""
 	Places a piece at a column and row
 	Takes a column, ex: 'A'	and a row, ex: 8
 	"""
-	
+
 	#Upper case letter
 	column = column.upper()
-	
-	
+
+
 	print(column, row)
 
 def prompt_move():
-	"""Prompts the user for a move until it is valid. 
+	"""Prompts the user for a move until it is valid.
 	Then places a piece on the board if the move is valid."""
-	
+
 	valid_move = False
 
 	#Only run when move is not valid
@@ -86,11 +84,9 @@ def prompt_move():
 		column = column_and_row[0]
 		row = column_and_row[1]
 
-		print(column + str(row))
-
 		#Check if the move is valid
 		valid_move = check_validity(column, row)
-		
+
 		#Alert the user
 		if not valid_move:
 			print("That is not a valid position!")
