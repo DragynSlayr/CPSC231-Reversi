4a8c1f22f52f28e613a90bc088ae641a705bd481
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-10-02 09:39:54 -0600

    Added comments

--- team_tasks/team_task3/Constants.py
+++ team_tasks/team_task3/Constants.py
@@ -1,20 +1,43 @@
-import turtle
-#Constants
+"""
+This file holds the constant values for the game
+These values are used for all drawing and sizing of the game
+"""
 
-NUM_OF_CELLS = 64
+import turtle
+import string
+
+#Grid information
 NUM_OF_ROWS = 8
 NUM_OF_COLUMNS = 8
+NUM_OF_CELLS = NUM_OF_ROWS * NUM_OF_COLUMNS
+
+#Window information
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
+
+#Cell information
 CELL_WIDTH = 50
 CELL_HEIGHT = 50
+
+#Arrays of letters and numbers for the columns and the rows
 COLUMN_LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 ROW_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8]
-X_OFFSET = WINDOW_WIDTH / 4
-Y_OFFSET = (WINDOW_HEIGHT * (2/3)) + (WINDOW_HEIGHT / 8)
-TITLE_LOCATIONX = WINDOW_WIDTH / 2
-TITLE_LOCATIONY = WINDOW_HEIGHT * 0.75
+
+#The offset of the first cell of the grid
+X_OFFSET = WINDOW_WIDTH / 4 #Evaluates to 200
+Y_OFFSET = (WINDOW_HEIGHT * (2/3)) + (WINDOW_HEIGHT / 8) #Evaluates to 475
+
+#Offset for the title
+TITLE_LOCATIONX = WINDOW_WIDTH / 2 #Evaluates to 400
+TITLE_LOCATIONY = WINDOW_HEIGHT * 0.75 #Evaluates to 450
+
+#The offset for the rules
 RULES_LINESTARTX = 0
-RULES_LINESTARTY = WINDOW_HEIGHT / 2
-RULES_LIST = WINDOW_HEIGHT / 2
+RULES_LINESTARTY = WINDOW_HEIGHT / 2 #Evaluates to 600
+
+#The y position of the rules
+RULES_LIST = WINDOW_HEIGHT / 2 #Evaluates to 300
+
+#The window and turtle
 WINDOW = turtle.Screen()
+TURTLE = turtle.Turtle()
