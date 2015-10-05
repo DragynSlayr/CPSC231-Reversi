b26904019022eaa246ab1a7bf83c0cb4968aab0b
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-10-05 09:44:35 -0600

    Commented

--- test_code/BoardClickMain.py
+++ test_code/BoardClickMain.py
@@ -1,7 +1,10 @@
+#Uses BoardClick module but calls it as "bc"
 import BoardClick as bc
 
 def main():
+    #Call the starting method of BoardClick
     bc.start()
 
+#Only run if main class
 if __name__ == "__main__":
     main()
