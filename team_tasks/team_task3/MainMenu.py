c7ac6c4c3a9c5b534ee3e0134a552fe8ba2dee4a
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-10-13 17:10:51 -0600

    Fixed typo, Loopend -> loopend, added final if check at the end of the game

--- team_tasks/team_task3/MainMenu.py
+++ team_tasks/team_task3/MainMenu.py
@@ -7,7 +7,7 @@
 #| _| `._____|_______|   \__/     |_______|| _| `._____|_______/    |__|     \______| /__/     \__\ |__|  |__| |_______|
 
 
-#Here we import the required files 
+#Here we import the required files
 import Constants
 import turtle
 import time
@@ -19,7 +19,7 @@ def MenuInput(text):
 	wn=Constants.WINDOW
 	userinput = wn.textinput("User Input", text)
 	return userinput
-	
+
 
 def show_rules():
 	text = Constants.TURTLE
@@ -30,7 +30,7 @@ def show_rules():
 	text.goto(Constants.TITLE_LOCATIONX, Constants.TITLE_LOCATIONY)
 	text.down()
 	text.write("RERVERSI GAME", False, align = "center", font = ("Arial", 50, "bold"))
-	
+
 	#Creates a list called rules, which is emppty for now
 	rules = []
 
@@ -44,45 +44,45 @@ def show_rules():
 	rules.append("When a player places a disk on the board, all the pieces that are between that player's newly placed disc \n and any of their previously placed discs, are turned to that player's color. \n")
 	rules.append("When placing your discs on your turn, there has to be at least one piece of the opposite colour between \n your placed piece and any of your previously placed pieces. Otherwise the move is not valid. ")
 	rules.append("The placed pieces have to make either a horizontal, vertical or diagonal line with the opposing player's discs to flip any pieces over.")
-	
+
 	#Move the turtle to the appropriate location for the rules output
 	line = Constants.RULES_LINESTARTY
 	text.up()
 	text.goto(Constants.TITLE_LOCATIONX, Constants.RULES_LINESTARTY)
 	text.down()
-	
-	
-	
+
+
+
 	#Loops for the number of rules in the list, which is what the len() function returns
 	for r in range(len(rules)):
 		#Print rules
 
-		
+
 		#Each loop moves the next rule down 30 units
 		line = line - 30
-		
+
 		text.up()
 		text.goto(Constants.RULES_LINESTARTX, line)
 		text.down()
-		
+
 		#The next line will print the rules
 		#We use \n to ensure it starts a new line
 		#str(r+1) is the number of the rule that is getting printed, ex				 1) "reversi...."
 		#False denotes whether or not the turtle is moveable
 		text.write("\n" + str(r + 1) + ") " + rules[r], False, align = "left", font = ("Arial", 10, "normal"))
-		
+
 		text.up()
 		text.goto(Constants.RULES_LINESTARTX, line)
 		text.down()
 
-	
+
 	#If the user choices 3, then the program will exit.
 	RulesChoice = MenuInput("Press any key to return, or press '3' to quit.")
 	if RulesChoice == '3' :
 		exit()
-	
 
-	
+
+
 
 
 #This prints out all of the main menu text
@@ -114,10 +114,10 @@ def MainMenu():
 	#Here the user makes an input, which is what the function returns
 	MenuChoice = MenuInput("Please make a choice:")
 	return MenuChoice
-	
+
 
 #This function is used in main, and will only show up if the user makes an invalid key entry
-	
+
 def InvalidChoice():
 	text = Constants.TURTLE
 	text.clear()
@@ -127,17 +127,17 @@ def InvalidChoice():
 	text.write("Invalid Choice", False, align = "center", font = ("Arial", 50, "bold"))
 	#Wait one second then continue
 	time.sleep(1)
-	
+
 
 def main():
 	#Declare the variable that is used to end the loop
 	loopend = False
-	
+
 	#This loop will always loop back to the main menu, prompting the user for a choice until they press play, or exit.
 	while loopend == False :
 		#We declare inRules to be False, which will prevent the ""invalidChoice"" functioning from running when the user presses 'any key' in the rules.
 		inRules= False
-		
+
 		#We get the user's choice
 		UserChoice = MainMenu()
 		if UserChoice == '1' :
@@ -145,28 +145,29 @@ def main():
 			#Here we set up the board and prompt them for a move
 			TurtleMove.setup()
 			TurtleMove.prompt_move()
-			
+
 			#So we must end the loop
-			Loopend = True
+			loopend = True
 		else:
 			#Check if the user wannts to see the rules or quit
 			if UserChoice == '2' :
 				show_rules()
-				
-				#update inRules to prevent the invalidChoice function from running in line 
+
+				#update inRules to prevent the invalidChoice function from running in line
 				inRules= True
-				
+
 			if UserChoice == '3':
 				#The user sucks and wants to quit
 				exit()
 		#Here we check if the user made a valid choice and dsiplay the appropriate text if they did not.
-		if UserChoice is not '1' or '2' or '3' :
+		if UserChoice is not '1' or '2' or '3':
 			#This if statement will prevent InvalidChoice from running when the user is viewing the rules
 			if inRules==False:
-				InvalidChoice()
-			
+				if loopend == False:
+					InvalidChoice()
 
-		
+
+
 
 #Run if main file
 if __name__ == "__main__":
@@ -174,7 +175,7 @@ if __name__ == "__main__":
 	Constants.WINDOW.setworldcoordinates(0, 0, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT)
 	Constants.WINDOW.clear()
 	main()
-	
+
 
 
 #Remember to comment out this exit once this file has been imported
