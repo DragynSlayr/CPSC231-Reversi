cdc762de1595ac07eda18deeae60570cade817ed
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-10-17 22:20:42 -0600

    Fixed tests and commented

--- team_tasks/team_task4/Part3.py
+++ team_tasks/team_task4/Part3.py
@@ -1,7 +1,7 @@
 #Write a function that takes a current game state as a parameter and returns true if the game is over.
 #It must detect if the board is full or if there are no further moves possible.
 import Constants
-import ConfigGenerator as cfg
+import BoardGenerator as cfg
 
 #Checks if the game is over
 #Params: game_state, A string representation of the board
@@ -12,25 +12,20 @@ def is_game_over(game_state):
 	count = 0
 	#Iterate through the game state
 	for s in game_state:
+		#Count non blank pieces
 		if s == Constants.PIECE_BLACK or s == Constants.PIECE_WHITE:
 			count += 1
 	return count == limit
 
 if __name__ == "__main__":
-	#Asks the user for input if not q then run tests
-	while (input("Press q to quit: ") != 'q'):
-		#Make sure the user wants this
-		if input("Enter 'PROCEED' to continue: ") == "PROCEED":
-			game_over = False
-			count = 0
-			#Tests can take between 1 and infinity generations, 1s+
-			while not game_over:
-				if count % 100 == 0:
-					print("TEST #%d" % count)
-				count += 1
-				game_state = cfg.generate()
-				game_over = is_game_over(game_state)
-			#Finally print a test that passed, all others failed
-			print(game_state + "\n\nHas resulted in a game over after %d generations!" % count)
-		else:
-			print("You saved a lot of time, congratulations!")
+	board = cfg.generate(True, True)
+	print(board, "\n%s" % is_game_over(board))
+
+	board = cfg.generate(True, False)
+	print(board, "\n%s" % is_game_over(board))
+
+	board = cfg.generate(False, True)
+	print(board, "\n%s" % is_game_over(board))
+
+	board = cfg.generate(False, False)
+	print(board, "\n%s" % is_game_over(board))
