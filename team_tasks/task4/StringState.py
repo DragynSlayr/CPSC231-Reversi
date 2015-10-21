b8c2ccb2a026de805417d1e9158fc8aecea1b0db
DavidKeizer <davidkeizer@shaw.ca>
2015-10-21 10:07:36 -0700

    Updated ScreenString

--- team_tasks/task4/StringState.py
+++ team_tasks/task4/StringState.py
@@ -68,135 +68,73 @@ def convert_column_number(column):
 #Returns Boolean depending on the local pieces
 	# - By local, it referes the pieces of in the immediate vicinity to the location on the grid
 def validate_local_location(P_gridString, P_location, colour):
+	
+	
+	
 	L_xy = TurtleMove.get_column_and_row(P_location)
 	L_x =  convert_column_number(L_xy[0]) #NOTE!!: This will be assigned 'invalid_range' if it is outside of range. This is done by the convert function
-	L_y = L_xy[1]
+	#Subtract one to bring it so 1 = 0, which simplifies later calulations
+	L_y = L_xy[1]-1
+	L_StringLocation = L_y*8 + L_x
+	 
 	#########
 	#|1|2|3|#
 	#|4|x|5|#
 	#|6|7|8|#	
 	#########
 	
-	L_gridSpot1 = 'UA'
-	L_gridSpot2 = 'UA'	
-	L_gridSpot3 = 'UA'
-	L_gridSpot4 = 'UA'
-	L_gridSpot5 = 'UA'
-	L_gridSpot6 = 'UA'
-	L_gridSpot7 = 'UA'
-	L_gridSpot8 = 'UA'
-	LocalGridString = L_gridSpot1+ L_gridSpot2 +L_gridSpot3+ L_gridSpot4 +L_gridSpot5+ L_gridSpot6 +L_gridSpot7+ L_gridSpot8 
 	
-	#Top left corner
-	if L_y ==1 and L_x == 0:
-		#Only locations can be B1, A2, or B2
-		L_gridSpot1 = 'U'
-		L_gridSpot2 = 'U'
-		L_gridSpot3 = 'U'
-		L_gridSpot4 = 'U'
-		L_gridSpot5 = 'B1'
-		L_gridSpot6 = 'U'
-		L_gridSpot7 = 'A2'
-		L_gridSpot8 = 'B2'
-		LocalGridString = L_gridSpot1+ L_gridSpot2 +L_gridSpot3+ L_gridSpot4 +L_gridSpot5+ L_gridSpot6 +L_gridSpot7+ L_gridSpot8 
-		
-	#Top right corner
-	if L_y == 1 and L_x == 7:
-		#Only locations can be G1, H1, or H2
-		L_gridSpot1 = 'U'
-		L_gridSpot2 = 'U'
-		L_gridSpot3 = 'U'
-		L_gridSpot4 = 'G1'
-		L_gridSpot5 = 'U'
-		L_gridSpot6 = 'G2'
-		L_gridSpot7 = 'H2'
-		L_gridSpot8 = 'U'
-		LocalGridString = L_gridSpot1+ L_gridSpot2 +L_gridSpot3+ L_gridSpot4 +L_gridSpot5+ L_gridSpot6 +L_gridSpot7+ L_gridSpot8 
 	
-	#Bottom left corner
-	if L_y == 8 and L_x == 0:
-		#Only locations can be A7, B7, or B8
-		L_gridSpot1 = 'U'
-		L_gridSpot2 = 'A7'
-		L_gridSpot3 = 'B7'
-		L_gridSpot4 = 'U'
-		L_gridSpot5 = 'B8'
-		L_gridSpot6 = 'U'
-		L_gridSpot7 = 'U'
-		L_gridSpot8 = 'U'
+	
+	#These locations will then be compared to what is stored in the string to check if any of the pieces is of the opposite colour 
+	
+	L_gridSpot1 = L_StringLocation -9
+	L_gridSpot2 = L_StringLocation -8	
+	L_gridSpot3 = L_StringLocation -7
+	L_gridSpot4 = L_StringLocation -1
+	L_gridSpot5 = L_StringLocation +1
+	L_gridSpot6 = L_StringLocation +7
+	L_gridSpot7 = L_StringLocation +8
+	L_gridSpot8 = L_StringLocation +9
+	
+	loopcount = 0
+	L_gridList=[L_gridSpot1,  L_gridSpot2, L_gridSpot3,  L_gridSpot4, L_gridSpot5, L_gridSpot6, L_gridSpot7, L_gridSpot8 ]
+	for L_gridSpot in L_gridList:
 		
+	#	print (L_gridSpot)
+	#	print(L_gridSpot <0)
 		
-	#Bottom right corner
-	if L_y == 8 and L_x == 7:
-		#Only locations can be G7, H7, or H8
-		L_gridSpot1 = 'G7'
-		L_gridSpot2 = 'H7'
-		L_gridSpot3 = 'U'
-		L_gridSpot4 = 'G8'
-		L_gridSpot5 = 'U'
-		L_gridSpot6 = 'U'
-		L_gridSpot7 = 'U'
-		L_gridSpot8 = 'U'
-		LocalGridString = L_gridSpot1+ L_gridSpot2 +L_gridSpot3+ L_gridSpot4 +L_gridSpot5+ L_gridSpot6 +L_gridSpot7+ L_gridSpot8 
+		if L_gridSpot <= 0:
+			L_gridList[loopcount] = 'U'
+	#		print("done")
 		
-	#Top edge
-	if L_y == 1:
-		if L_x != 1 and L_x != 7:
-			L_gridSpot1 = 'U'
-			L_gridSpot2 = 'U'
-			L_gridSpot3 = 'U'
-			L_gridSpot4 = str(L_x -1)
-			L_gridSpot5 = str(L_x+1)
-			L_gridSpot6 =str( L_x +7)
-			L_gridSpot7 = str(L_x +8)
-			L_gridSpot8 = str(L_x +9)
-			LocalGridString = L_gridSpot1+ L_gridSpot2 +L_gridSpot3+ L_gridSpot4 +L_gridSpot5+ L_gridSpot6 +L_gridSpot7+ L_gridSpot8 
+		if L_gridSpot >=64:
+			L_gridList[loopcount] = 'U'
 			
-	#Bottom edge
-	if L_y ==8:
-		if L_x != 1 and L_x != 7:
-			L_gridSpot1 = str(L_x -9)
-			L_gridSpot2 = str(L_x -8)
-			L_gridSpot3 = str(L_x -7)
-			L_gridSpot4 = str(L_x -1)
-			L_gridSpot5 = str(L_x+1)
-			L_gridSpot6 = 'U'
-			L_gridSpot7 = 'U'
-			L_gridSpot8 = 'U'
-			LocalGridString = L_gridSpot1+ L_gridSpot2 +L_gridSpot3+ L_gridSpot4 +L_gridSpot5+ L_gridSpot6 +L_gridSpot7+ L_gridSpot8 
+		if L_gridSpot%8 == 1:
+			L_gridList[loopcount ] = 'U'
+			########################################################################
+			#This is not correctly detecting if it is off the edge left or right!!!!
+			print("AHA")
+			print(L_gridSpot)
+			print(loopcount)
+		loopcount = loopcount +1	
+		
+	#Uncomment to test
+	
+	#for L_gridSpot in L_gridList:
+		#print(L_gridSpot)
 			
-	#Left edge
-	if L_x == 0:
-		if L_y != 1 and L_y != 8:
-			L_gridSpot1 = 'U'
-			L_gridSpot2 = str(L_x -8)
-			L_gridSpot3 = str(L_x -7)
-			L_gridSpot4 = 'U'
-			L_gridSpot5 = str(L_x+1)
-			L_gridSpot6 = 'U'
-			L_gridSpot7 = str(L_x+8)
-			L_gridSpot8 = str(L_x+9)
-			LocalGridString = L_gridSpot1+ L_gridSpot2 +L_gridSpot3+ L_gridSpot4 +L_gridSpot5+ L_gridSpot6 +L_gridSpot7+ L_gridSpot8 
-	
-	#Right edge
-	if L_x ==7:
-		if L_y != 1 and L_y != 8:
-			L_gridSpot1 = str(L_x -9)
-			L_gridSpot2 = str(L_x -8)
-			L_gridSpot3 = 'U'
-			L_gridSpot4 = str(L_x -1)
-			L_gridSpot5 = 'U'
-			L_gridSpot6 = str(L_x +7)
-			L_gridSpot7 = str(L_x+8)
-			L_gridSpot8 = 'U'
-			LocalGridString = L_gridSpot1+ L_gridSpot2 +L_gridSpot3+ L_gridSpot4 +L_gridSpot5+ L_gridSpot6 +L_gridSpot7+ L_gridSpot8 
+		
 			
-	#Any normal spot
-	#for letter in LocalGridString:
-	#	if letter == 'U' or 
 	
 	
+	
+	
+	
+															#Eventually 'null' will be replaced by the colour of the piece
+validate_local_location(initialize_grid_string_start(), 'A1', 'null')	
 #print(len(initialize_grid_string_start()))	
-print(validate_move_location('NNNNNNNNBBBBWWWWNNNNNNNNNNNBWNNNNNNWBNNNNNNNNNNNNNNNNNNNNNNNNNNN', 'H8'))
+#print(validate_move_location('NNNNNNNNBBBBWWWWNNNNNNNNNNNBWNNNNNNWBNNNNNNNNNNNNNNNNNNNNNNNNNNN', 'H8'))
 
 
