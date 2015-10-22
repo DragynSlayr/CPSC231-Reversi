15f488ce659e5b2622e864872c24d1f4efcfabdc
kyle.hinton2 <kyle.hinton2@zone43-eb.cs.ucalgary.ca>
2015-10-22 11:57:34 -0600

    StringInterpreter uploaded

--- team_tasks/task4/StringInterpret.py
+++ team_tasks/task4/StringInterpret.py
@@ -3,18 +3,21 @@ import TurtleMove
 import ReversiGrid
 
 def StringToPiece(token, i):
-        for y in range(len(Constants.COLUMN_LETTERS)):
-                for x in range(len(Constants.ROW_NUMBERS)):
-                        x_coord = Constants.COLUMN_LETTERS[x]
-                        y_coord = Constants.ROW_NUMBERS[y]
-                        if token[i] == "N":
-                                TurtleMove.place_piece(x_coord, y_coord, "Blue")
-                        elif token[i] == "B":
-                                TurtleMove.place_piece(x_coord, y_coord, "Black")
-                        elif token[i] == "W":
-                                TurtleMove.place_piece(x_coord,y_coord, "White")
-                        i = i + 1
+	for y in range(len(Constants.COLUMN_LETTERS)):
+		for x in range(len(Constants.ROW_NUMBERS)):
+			x_coord = Constants.COLUMN_LETTERS[x]
+			y_coord = Constants.ROW_NUMBERS[y]
+			if token[i] == "N":
+				TurtleMove.place_piece(x_coord, y_coord, "Blue")
+			elif token[i] == "B":
+				TurtleMove.place_piece(x_coord, y_coord, "Black")
+			elif token[i] == "W":
+				TurtleMove.place_piece(x_coord,y_coord, "White")
+			i = i + 1
 
+
+#This function takes the current game state as a token and the next move and updates the gamestate token based on the move.
+#
 def StringInterpret(token, NewMove):
         column = NewMove[0]
         row = int(NewMove[1])
@@ -28,19 +31,31 @@ def StringInterpret(token, NewMove):
         
         StringToPiece(NewToken, 0)
 
-        print(token[StringGrab])
-        print(ColumnIDX, "Columnidx")
-        print(RowIDX, "Rowidx")
-        print(column, "Column")
-        print(row, "Row")
-        print(StringGrab)
-        print(NewToken)
+        print(token[StringGrab]) #Prints the character at the index [StringGrab]
+        print(ColumnIDX, "Columnidx") # Column index number from letters
+        print(RowIDX, "Rowidx")	#Row index number from numbers
+        print(column, "Column")	#Prints the Column Letter
+        print(row, "Row") #Prints the Row Number
+        print(StringGrab, "Index number of new move") #Takes the move and converts it to the index number for the string 
+        print(NewToken)	#Prints the NewToken with the character at index.StringGrab in place
+        return NewToken	#returns the NewToken, The updated move.
 
 
 if __name__ == "__main__":
         TurtleMove.setup()
-        StringToPiece(("NWWBBWBB" * 8), 0)
-        StringInterpret(("NWWBBWBB" * 8), "C7")
+ #       StringToPiece(("NWWBBWBB" * 8), 0)
+        NewToken = StringInterpret(("NNNNNNNN" * 8), input("Move?"))
+        NewToken = StringInterpret((NewToken), input("Move"))
+        NewToken = StringInterpret((NewToken), input("Move?"))
+        """
+        NewToken = StringInterpret((NewToken), "B3")
+        NewToken = StringInterpret((NewToken), "B4")
+        NewToken = StringInterpret((NewToken), "C2")
+        NewToken = StringInterpret((NewToken), "C4")
+        NewToken = StringInterpret((NewToken), "D2")
+        NewToken = StringInterpret((NewToken), "D3")
+        NewToken = StringInterpret((NewToken), "D4")
+        """
         Constants.WINDOW.exitonclick()
 
         
