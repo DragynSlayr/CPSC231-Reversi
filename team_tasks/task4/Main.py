c9d8781c01e396f10951ef8181e10853c64695a4
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-10-24 23:25:57 -0600

    Removed some print statements

--- team_tasks/task4/Main.py
+++ team_tasks/task4/Main.py
@@ -15,20 +15,17 @@ def Main():
 	Gamestate = StringInterpret.StringInterpret(Gamestate, "E4", 1)
 	Gamestate = StringInterpret.StringInterpret(Gamestate, "E5", 2)
 	Gamestate = StringInterpret.StringInterpret(Gamestate, "D5", 3)
-	print("before loop")
 	move_num = 4
 	while VictoryStatus.is_game_over(Gamestate) == False:
 		move = StringMove.get_move(Gamestate)
 		Gamestate = StringInterpret.StringInterpret(Gamestate, move, move_num)
 		move_num += 1
-		print("in loop")
 	if PlayerVictory.PlayerWon(Gamestate) == True:
 		#Black Won!
 		print("Black Won")
-
 	else:
-		print("White Won!")
 		#White Won!
+		print("White Won!")
 
 if __name__ == "__main__":
 	Main()
