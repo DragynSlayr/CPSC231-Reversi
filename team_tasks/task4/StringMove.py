ab964aafecab42e2059c01c69e2a1c36661084c4
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-10-26 09:16:20 -0600

    Now detects if a move is empty, to prevent a crash. Also commented

--- team_tasks/task4/StringMove.py
+++ team_tasks/task4/StringMove.py
@@ -4,8 +4,6 @@ import Constants
 import ReversiGrid
 import StringInterpret
 
-
-
 #Function returns a string that represents the initial grid state
 #Takes no parameters
 def initialize_grid_string_start():
@@ -19,6 +17,8 @@ def get_Char(P_string, P_location):
 	l_location = P_string[P_location-1]
 	return l_location
 
+#Converts a column letter to an index number
+#Takes a column letter as a parameter
 def convert_column_number(column):
 	columnAll = 'ABCDEFGH'
 	loopcount = 0
@@ -40,12 +40,9 @@ def validate_move_location(P_gridString, P_location):
 
 	#Check if the location is on the board,
 	if L_x == 'invalid_range':
-		print(L_y)
-		print("x out of Range")
 		return False
 
 	if 0<= L_y > 8:
-		print ("y out of range")
 		return False
 
 	#Checks if the location is occupied already
@@ -56,22 +53,32 @@ def validate_move_location(P_gridString, P_location):
 		#If the location is on the board return True.
 		return True
 
+#Displays a window that asks the user for input
+#Takes a message as input
+#Returns the user's input
 def MenuInput(text):
-	wn=Constants.WINDOW
+	wn = Constants.WINDOW
 	userinput = wn.textinput("User Input", text)
 	return userinput
+
 #Function asks the user for a move and then validates it
-#Takes the current string state
+#Takes the current string state and a message to prompt the user
 #Function returns the move if valid
-
-
 def get_move(P_StringState, message):
 	L_userMove = MenuInput(message)
 	if L_userMove == None:
-		return None
+		return "invalid"
 	else:
-		while  validate_move_location(P_StringState, L_userMove) == False:
+		validMove = False
+		while not validMove:
+			#Recursively ask the user for their move until it is valid
 			L_userMove = get_move(P_StringState, "Invalid Location: Re-Enter")
+
+			#Check if the move is not blank
+			if L_userMove != "":
+				validMove = validate_move_location(P_StringState, L_userMove)
+			else:
+				validMove = False
 		return L_userMove
 
 #Function updates the string to include the move
@@ -79,15 +86,19 @@ def get_move(P_StringState, message):
 #Function returns updated grid state
 #Example: update_string_state(initialize_grid_string_start(), 'H8', 'B')
 def update_string_state(P_gridString, P_location, colour):
+	#Get column and row from location
 	L_xy = TurtleMove.get_column_and_row(P_location)
+
+	#Extract column and row from above
 	L_x =  convert_column_number(L_xy[0]) #NOTE!!: This will be assigned 'invalid_range' if it is outside of range. This is done by the convert function
 	L_y = L_xy[1] - 1 #This needs to start at 0 in order to match the for loop
 
+	#Convert column and row to an index
 	L_updateLocation = (8*L_y + L_x)
 	F_LetterTracker = 0
 	L_updatedString = ''
 
-
+	#Iterate through gamesate and accumulate an updated board
 	for letter in P_gridString:
 		F_LetterTracker = F_LetterTracker +1
 		if F_LetterTracker == L_updateLocation:
