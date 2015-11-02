bafd4e8732bad2646f9d59496b5e207023ef427a
KyleHinton <kyle-hinton@hotmail.com>
2015-11-01 20:54:53 -0700

    Still can't get other directions to work with string. I'm probably doing it wrong. Added Comments.

--- test_code/ValidMove.py
+++ test_code/ValidMove.py
@@ -1,20 +1,16 @@
 
 
-MoveToString = 27                   
+#MoveToString = 27                   
                                     #                              
-token = "WNNNNBBNNBNNBNNNNNBNBWBBBBBWBBBWNNBNBNNNNBNNNBNNBNNNNNBNNNNNNNNW"
+token = "WNBBBBBBWBNBBBBBBBBBBWBBNBBWBBBWNNBWWBNNNBNNNBNNBNNNNNBNNNNNNNNW"
 TurnColour = "W"
 OtherColour = "B"
-WEST = -1
-NORTHWEST = -7
-n = 1
 
 # if the piece next to the current move is the OtherColour:  (8 times one for each direction)
 #while the index does not equal the OtherColour add that index to the turn's string. if the index finds the turn colour, turn all the colours up to that index into the TurnColour.
 #if token[MoveToString - 1] == OtherColour or token[MoveToString - 9] == OtherColour or token[MoveToString - 8] == OtherColour or token[MoveToString - 7] == OtherColour or token[MoveToString + 1] == OtherColour
 #or token[MoveToString + 9] == OtherColour or token[MoveToString + 8] == OtherColour or token[MoveToString + 7] == OtherColour:
-	
-n = 1
+
 
 def CheckWest(token, MoveToString):
 
@@ -24,23 +20,23 @@ def CheckWest(token, MoveToString):
 	NORTHWEST = -7
 	n = 1
 
-	while token[MoveToString + (WEST)] == OtherColour:
-		print (n, "n Number of other colours")
-		print (MoveToString + (WEST), "index of OtherColour")
-		MoveToString = MoveToString + (WEST)
-		if token[MoveToString] == "N":
+	while token[MoveToString + (WEST)] == OtherColour:			#While the next token is the OtherColour
+		print (n, "n Number of other colours")					#Print the number of times it crosses the OtherColour
+		print (MoveToString + (WEST), "index of OtherColour")	#Print the index number of the OtherColour
+		MoveToString = MoveToString + (WEST)					#Moves the checker up. Subtracts one from MoveToString
+		if token[MoveToString] == "N":							#Invalid move if the next letter on the string is N
 			print("that move is invalid")
-		n = n + 1
+		n = n + 1												#Adds one to the count of the other colours.
 
-		if token[MoveToString + (WEST)] == TurnColour:
-			NewToken = token[:MoveToString] + (TurnColour * n) + token[MoveToString + (WEST) + n + 1:]
-			print (token)
-			print(NewToken)
-			print (n, "How many times we have to print the TurnColour's letter.")
+		if token[MoveToString + (WEST)] == TurnColour:			#If the next token is the TurnColour
+			NewToken = token[:MoveToString] + (TurnColour * n) + token[MoveToString + (WEST) + n + 1:]	#Create this string
+			print (token)										#Print original token
+			print(NewToken)										#Print NewToken
+			print (n, "How many times we have to print the TurnColour's letter.")	#Number of tokens that will be changed.
 
 
 			
-def CheckEAST(token, MoveToString):
+def CheckEast(token, MoveToString):
 	TurnColour = "W"
 	OtherColour = "B"
 	WEST = -1
@@ -48,20 +44,71 @@ def CheckEAST(token, MoveToString):
 	EAST = 1
 	n = 1
 
-	while token[MoveToString + (EAST)] == OtherColour:
-		print (n, "n Number of other colours")
-		print (MoveToString + (EAST), "index of OtherColour")
-		MoveToString = MoveToString + (EAST)
-		if token[MoveToString] == "N":
+	while token[MoveToString + (EAST)] == OtherColour:			#While the next token is the OtherColour
+		print (n, "n Number of other colours")					#Print the number of times it crosses the OtherColour
+		print (MoveToString + (EAST), "index of OtherColour")	#Print the index number of the OtherColour
+		MoveToString = MoveToString + (EAST)					#Moves the checker up. Adds one to MoveToString
+		if token[MoveToString] == "N":							#Invalid move if the next letter on the string is N
 			print("that move is invalid")
-		n = n + 1
+		n = n + 1												#Adds one to the count of the other colours.
 
-		if token[MoveToString + (EAST)] == TurnColour:
-			NewToken = token[:MoveToString - n + 1] + (TurnColour * n) + token[MoveToString + 1:]
-			print (token)
-			print(NewToken)
-			print (n, "How many times we have to print the TurnColour's letter.")
+		if token[MoveToString + (EAST)] == TurnColour:			#If the next token is the TurnColour
+			NewToken = token[:MoveToString - n + 1] + (TurnColour * n) + token[MoveToString + 1:]	#Create this string
+			print (token)										#Print original token
+			print(NewToken)										#Print NewToken
+			print (n, "How many times we have to print the TurnColour's letter.")	#Number of tokens that will be changed.
+			
+			
+
+def CheckNorthWest(token, MoveToString):
+	TurnColour = "W"
+	OtherColour = "B"
+	WEST = -1
+	NORTHWEST = -9
+	EAST = 1
+	n = 1
+	
+	PlaceHolder = ""
+	NewToken = ""
 
 
-CheckWest(token, 27)
-CheckEAST(token, 27)
+	while token[MoveToString + (NORTHWEST)] == OtherColour:			#While the next index at nine less equals the OtherColour
+		print (n, "n Number of other colours")						#Print the number of the other colours it has counted so far
+		print (MoveToString + (NORTHWEST), "index of OtherColour")	#Prints what MoveToString will become
+		MoveToString = MoveToString + NORTHWEST						#Makes MoveToString equal to 9 less than what it was. Subtracts 9
+		if token[MoveToString] == "N":								#If the next token is an N the move is invalid.
+			print("that move is invalid")
+		n = n + 1													#add one to the number of tokens that are the other colours
+
+	if token[MoveToString + (NORTHWEST)] == TurnColour:			#If the next token is the Turn colour
+		print (n)												#Print the number of OtherColour tokens there are
+		MoveToString = MoveToString + NORTHWEST					#Move the index up to the location of the indext of TurnColour across from the move.
+		while n != 0:											#while the number of other colours checked does not equal 0
+
+			PlaceHolder = TurnColour + token[MoveToString + 1:MoveToString - NORTHWEST]
+			NewToken = NewToken + PlaceHolder					#STRING:NewToken is the Previous Placeholders + the current Placeholder
+			MoveToString = MoveToString - NORTHWEST				#Adds 9 to MoveToString to change the next piece
+			n = n - 1											#Counts down to zero based on the number of pieces counted
+			print (n , "Number of tokens left")					#Number of tokens left to change
+			print (PlaceHolder, "PlaceHolder")					#Placeholder Token
+			print (NewToken, "NewToken")						#Entire NewToken
+		if n == 0:												#When the number of pieces to change is zero
+			print ("        !#       !#       !#       #")		#Measurement test # is for test one ! for test 2
+			print ("01234567890123456789012345678901234567890")	#Measurement to count characters
+			print (token)										#print the old token for comparison
+			print (NewToken[:MoveToString] + token[MoveToString:], "NEWTOKENNEW")	#print the NewToken with the end of the last token. THERE IS A PROBLEM HERE. IF THE END PIECE IS NOT AT 0 IT WONT PRINT THE FIRST PART OF STRING.
+			print (NewToken, "RealNewToken")					#Print the RealNewToken that was created.
+				
+
+
+
+#CheckWest(token, 27)
+#CheckEast(token, 27)
+print ("___________________________")
+print ("TEST 1 TEST 1 TEST 1 TEST 1")
+print (" ")
+CheckNorthWest(token, 27)
+print ("___________________________")
+print ("TEST 2 TEST 2 TEST 2 TEST 2")
+print (" ")
+CheckNorthWest(token, 36)
