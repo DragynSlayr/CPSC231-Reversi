2b36037b6e0cb659949d9f7b20454880aea0197a
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-11-11 13:58:59 -0700

    Now supports clicking

--- team_tasks/task6/MainMenu.py
+++ team_tasks/task6/MainMenu.py
@@ -1,164 +1,160 @@
-#This file has the method, main, to show and take input for the main menu
-
+#This file has the method, main,
+#to show and take input for the main menu
 import Constants
 import turtle
 import time
 import TurtleMove
-
-#This function prompts a user for an input
-#Only parameter is the text that is to be displayed with the prompt
-#Returns the user's unput
-def inputRequest(text):
-	wn = Constants.WINDOW
-	user_input = wn.textinput("User Input", text)
-	return user_input
+import Rules
+import sys
+import Main
 
 #Moves a turtle to a location without making a line
-#Params: text, The turtle
+#Params: turtle, The turtle
 #		 x, The x location
 #		 y, The y location
 #Returns: None
-def moveTurtle(text, x, y):
-	text.up()
-	text.goto(x, y)
-	text.down()
+def moveTurtle(turtle, x, y):
+	turtle.up()
+	turtle.goto(x, y)
+	turtle.down()
 
-#Function displays the rules on the window
-#The user is given the option to quit or return to the main menu
-#Returns nothing and takes a turtle as a parameter
-def displayRules(text):
-	#Move to the location for the title
-	text.clear()
-	moveTurtle(text, Constants.TITLE_LOCATIONX, Constants.TITLE_LOCATIONY)
+#Draws a button and labels it
+#Params: turtle, The turtle to use for the drawing
+#		 x, The x position of the top left of the button
+#		 y, The y position of the top left of the button
+#		 label, The word to write on the button
+#Returns: None
+def drawButton(turtle, x, y, label):
+	#Store the original line width
+	size = turtle.pensize()
 
-	#Draw the title text
-	text.write("REVERSI GAME", False, align = "center", font = ("Arial", 50, "bold"))
+	#Change the line width
+	turtle.pensize(2)
 
-	#Initialize a list for holding the rules
-	rules = []
+	#Draw the button
+	moveTurtle(turtle, x, y)
+	for i in range(4):
+		turtle.forward(Constants.BUTTON_WIDTH)
+		turtle.right(90)
+		turtle.forward(Constants.BUTTON_HEIGHT)
+		turtle.right(90)
 
-	#Add rules to list
-	rules.append("Reversi is a 2 player game that is played on an 8 x 8 grid. \n \n")
-	rules.append("The game discs have both a black and white side. \n \n")
-	rules.append("To win you have to have the majority of the discs changed to your color at the end of the game. \n \n")
-	rules.append("The game ends when there are no more possible moves to make. \n \n ")
-	rules.append("The game can end before the entire grid is filled. \n \n ")
-	rules.append("Players take turns placing discs, with their color facing up. \n \n ")
-	rules.append("When a player places a disk on the board, all the pieces that are between that player's newly placed disc \n and any of their previously placed discs, are turned to that player's color. \n")
-	rules.append("When placing your discs on your turn, there has to be at least one piece of the opposite colour between \n your placed piece and any of your previously placed pieces. Otherwise the move is not valid. ")
-	rules.append("The placed pieces have to make either a horizontal, vertical or diagonal line with the opposing player's discs to flip any pieces over.")
+	#Move to the center of the button and write a word
+	moveTurtle(turtle, x + (Constants.BUTTON_WIDTH / 2), y - (Constants.BUTTON_HEIGHT * (3/5)))
+	turtle.write(label, False, "center", font = ("Arial", 15, "bold"))
 
-	#Move the turtle to the appropriate location for the rules output
-	line = Constants.RULES_LINESTARTY
-	moveTurtle(text, Constants.TITLE_LOCATIONX, Constants.RULES_LINESTARTY)
+	#Change the width to normal
+	turtle.pensize(size)
 
-	#Go through the rules list
-	for r in range(len(rules)):
-		#Each loop moves the next rule down 30 units
-		line = line - 30
+#Draws the main menu
+#Params: None
+#Returns: None
+def showMenu():
+	#Get the turtle
+	turtle = Constants.TURTLE
 
-		#Move the turtle
-		moveTurtle(text, Constants.RULES_LINESTARTX, line)
-
-		#Format the line
-		rule = "\n" + str(r + 1) + ") " + rules[r]
-
-		#Draw the rule
-		text.write(rule, False, align = "left", font = ("Arial", 10, "normal"))
-
-		#Move back to the start of the line
-		moveTurtle(text, Constants.RULES_LINESTARTX, line)
-
-	#Prompt the user for a choice
-	user_choice = inputRequest("Press any key to return, or press '3' to quit.")
-	if user_choice == '3':
-		exit()
-
-#This prints out all of the main menu text
-#Takes a turtle as a parameter
-#Returns the user's choice
-def mainMenu(text):
 	#Move to the title location
-	text.clear()
-	moveTurtle(text, Constants.TITLE_LOCATIONX, Constants.TITLE_LOCATIONY)
+	turtle.clear()
+	moveTurtle(turtle, Constants.TITLE_LOCATIONX, Constants.TITLE_LOCATIONY)
 
 	#Write the title to the screen
-	text.write("REVERSI GAME", False, align = "center", font = ("Arial", 50, "bold"))
+	turtle.write("REVERSI GAME", False, align = "center", font = ("Arial", 50, "bold"))
 
 	#List of options
-	options = ["1. Play", "2. Rules", "3. Exit"]
+	options = ["Rules", "Play", "Exit"]
 
-	#Draw the options
-	for i in range(len(options)):
-		#Move turtle down
-		moveTurtle(text, Constants.TITLE_LOCATIONX, Constants.RULES_LIST - (i *  20))
+	#Draw all the buttons
+	drawButton(turtle, Constants.RULES_BUTTON_LEFT_X, Constants.BUTTON_TOP_Y, options[0])
+	drawButton(turtle, Constants.PLAY_BUTTON_LEFT_X, Constants.BUTTON_TOP_Y, options[1])
+	drawButton(turtle, Constants.EXIT_BUTTON_LEFT_X, Constants.BUTTON_TOP_Y, options[2])
 
-		#Write option
-		text.write(options[i], False, align = "center", font = ("Arial", 10, "normal"))
+#Checks if a click is in the button area
+#Params: x, The x location of the click
+#		 y, The y location of the click
+#Returns: True if the click was in the button area, False otherwise
+def isButtonClicked(x, y):
+	#Check if x and y are within the button area
+	x_valid = x >= Constants.RULES_BUTTON_LEFT_X and x <= Constants.EXIT_BUTTON_RIGHT_X
+	y_valid = y >= Constants.BUTTON_BOTTOM_Y and y <= Constants.BUTTON_TOP_Y
 
-	#Here the user makes a choice, which is what the function returns
-	menu_choice = inputRequest("Please make a choice:")
-	return menu_choice
+	#Return the result
+	return x_valid and y_valid
 
+#Checks if the x is over the rules button
+#Params: x, The x location to check
+#Returns: True if in the rules button, False otherwise
+def isRulesButton(x):
+	return x >= Constants.RULES_BUTTON_LEFT_X and x <= Constants.RULES_BUTTON_RIGHT_X
 
-#Function displays the 'Invalid Choice' screen, and then waits a second
-#Takes a turtle and returns nothing
-def invalidChoice(text):
-	#Move to the title location
-	text.clear()
-	moveTurtle(text, Constants.TITLE_LOCATIONX, Constants.TITLE_LOCATIONY)
+#Checks if the x is over the play button
+#Params: x, The x location to check
+#Returns: True if in the play button, False otherwise
+def isPlayButton(x):
+	return x >= Constants.PLAY_BUTTON_LEFT_X and x <= Constants.PLAY_BUTTON_RIGHT_X
 
-	#Draw the line
-	text.write("Invalid Choice", False, align = "center", font = ("Arial", 50, "bold"))
+#Checks if the x is over the exit button
+#Params: x, The x location to check
+#Returns: True if in the exit button, False otherwise
+def isExitButton(x):
+	return x >= Constants.EXIT_BUTTON_LEFT_X and x <= Constants.EXIT_BUTTON_RIGHT_X
 
-	#Wait one second then continue
-	time.sleep(1)
+#Gets the index of the button that is clicked
+#Params: x, The x location of the click
+#Returns: The index of the button, -1 if not a button
+def getClickedButton(x):
+	if isRulesButton(x):
+		return 0
+	elif isPlayButton(x):
+		return 1
+	elif isExitButton(x):
+		return 2
+	else:
+		return -1
+
+#Checks if a button is pushed and then responds
+#Params: x, The x position of the click
+#		 y, The y position of the click
+#Return: None
+def makeChoice(x, y):
+	#Check if a button is pressed
+	if isButtonClicked(x, y):
+		#Get the index of the clicked button
+		button = getClickedButton(x)
+
+		#Get the window
+		wn = Constants.WINDOW
+
+		#Store options
+		options = [Rules.show, Main.main, sys.exit]
+
+		#Call the desired method
+		if button >= 0:
+			options[button]()
 
 #Function shows the main menu and will show the desired menu
 #Function will not continue unless the user press's play
-#Returns nothin and takes no parameters
+#Returns nothing and takes no parameters
 def main():
+	#Get the window
+	wn = Constants.WINDOW
+
 	#Set up the window
-	Constants.WINDOW.setup(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT)
-	Constants.WINDOW.setworldcoordinates(0, 0, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT)
-	Constants.WINDOW.clear()
+	wn.setup(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT)
+	wn.setworldcoordinates(0, 0, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT)
+	wn.clear()
+	wn.tracer(10)
 
-	#Get the turtle
-	text = Constants.TURTLE
+	#Draw the main menu
+	showMenu()
 
-	#Declare the variable that is used to end the loop
-	end_loop = False
+	#Update the window
+	wn.update()
 
-	#This loop will always loop back to the main menu, prompting the user for a choice until they press play, or exit.
-	while end_loop == False :
-		#We declare user_in_rules to be False, which will prevent the ""invalidChoice"" functioning from running when the user presses 'any key' in the rules.
-		user_in_rules = False
-
-		#We get the user's choice
-		user_choice = mainMenu(text)
-
-		if user_choice == '1' :
-			#User wants to play,
-			#so we must end the loop
-			end_loop = True
-		else:
-			#Check if the user wannts to see the rules or quit
-			if user_choice == '2' :
-				displayRules(text)
-
-				#update user_in_rules to prevent the invalidChoice function from running in line
-				user_in_rules = True
-
-			if user_choice == '3':
-				#The user sucks and wants to quit
-				exit()
-				
-		#Here we check if the user made a valid choice and dsiplay the appropriate text if they did not.
-		if user_choice is not '1' or '2' or '3':
-			#This if statement will prevent invalidChoice from running when the user is viewing the rules
-			if user_in_rules == False:
-				if end_loop == False:
-					invalidChoice(text)
+	#Allow clicking
+	wn.onclick(makeChoice)
+	wn.onkey(wn.bye, "space")
+	wn.listen()
+	wn.mainloop()
 
 #Run if main file
 if __name__ == "__main__":
