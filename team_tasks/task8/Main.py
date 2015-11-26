a029df91c2bccb8a51e4965c3b4337eee974dd58
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-11-26 08:36:19 -0700

    Shortened Main.py

--- team_tasks/task8/Main.py
+++ team_tasks/task8/Main.py
@@ -5,37 +5,26 @@ import ListInterpret
 import Constants
 import BoardClick
 import random
-import Converter
-import ListUpdater
-
 
 def main():
 	#Set up the other classes
 	ReversiGrid.main()
 
-	#Use a blank starting board
-	game_state = Converter.toList("N" * 64)
+	#Get the starting board config
+	game_state = Constants.GAME_STATE_START
 
-	#Place the pieces from the random board
+	#Place the pieces from the starting board
 	ListInterpret.stringToPiece(game_state)
 
-	#Place starting config
-	game_state = ListInterpret.stringInterpret(game_state, "D4", 0)
-	game_state = ListInterpret.stringInterpret(game_state, "E4", 1)
-	game_state = ListInterpret.stringInterpret(game_state, "E5", 2)
-	game_state = ListInterpret.stringInterpret(game_state, "D5", 3)
-	
-	game_state = Constants.GAME_STATE_START
+	#The number of moves made so far
 	move_num = 0
-	
+
 	#Determine who goes first
-	rand_num = random.randint(0, 1)
-	isPlayerMove = rand_num == 0
+	isPlayerMove = random.randint(0, 1) == 0
 
 	#Start listening for clicks on a board,
 	#BoardClick takes over the logic from here
 	BoardClick.run(game_state, move_num, isPlayerMove)
-
 
 if __name__ == "__main__":
 	MainMenu.main()
