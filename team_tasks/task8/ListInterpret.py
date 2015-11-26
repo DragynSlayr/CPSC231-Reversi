fd027a98fc900fb34e1f71adbdf1fe5adb8b1b34
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-11-26 08:56:16 -0700

    Removed unused imports

--- team_tasks/task8/ListInterpret.py
+++ team_tasks/task8/ListInterpret.py
@@ -67,32 +67,4 @@ def stringInterpret(game_state, NewMove, turn):
 
 		TurtleMove.placePiece(column, row, color)
 
-		if __name__ == "__main__":
-			#TESTING TESTING TESTING
-			##########################
-			print("Testing")
-			print("turn ", turn)
-			print(column_IDX, "column_IDX") # Column index number from letters
-			print(row_IDX, "row_IDX")	#Row index number from numbers
-			print(column, "Column")	#Prints the Column Letter
-			print(row, "Row") #Prints the Row Number
-			print(game_state[move_to_string], ": Character at the move's index number.") #Prints the character at the index [move_to_string]
-			print(move_to_string, "Index number of new move") #Takes the move and converts it to the index number for the string
-			print(new_state)	#Prints the new_state with the character at index.move_to_string in place
-			print()
-			##########################
 		return new_state	#Return the updated game state
-
-#Sets up the board using the pieces and reading through the string.
-def setup2():
-	new_state = stringInterpret(("NNNNNNNN" * 8), "D4", -2)
-	new_state = stringInterpret((new_state), "E4", -1)
-	new_state = stringInterpret((new_state), "D5", -1)
-	new_state = stringInterpret((new_state), "E5", -2)
-	for turn in range(64):
-		new_state = stringInterpret((new_state), input("Move?"), turn + 1)
-
-if __name__ == "__main__":
-		TurtleMove.setup()
-		setup2()
-		Constants.WINDOW.exitonclick()
