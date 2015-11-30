eb432db7a13099b11c5d9b27cea5c63217016d45
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-11-30 15:41:07 -0700

    Shortened two conversion methods and incorporated constants

--- team_tasks/task8/listUpdater.py
+++ team_tasks/task8/listUpdater.py
@@ -3,74 +3,53 @@ import stringInterpret
 import reversiGrid
 import converter
 import turtleMove
+import constants
 
 #Converts the location into a list of ints that represent the x y coordinates
 #Only parameter is the location as a string, ex 'A7' or 'H8'
 #Returns a list of ints: ex [1, 7] or [8, 8]
 def convertMove(location):
-	#Create a temporary list that the function will work with
-	localation_List = []
-	for letter in location:
-		localation_List.append(letter)
+	letter = location[0]
+	number = int(location[1])
 
-	#Convert the y coordinate in an integer
-	temp = localation_List[1]
-	localation_List[1] = int(temp)
-	
-	#Declare necessary variables in preporation for x conversion
-	x = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-	counter = 1
-	row_IDX = 1
-	
-	#Convert the x by finding the index in the basic list x
-	for i in x:
-		if localation_List[0] == i:
-			row_IDX = counter
-		counter = counter+1
+	column = constants.COLUMN_LETTERS.index(letter) + 1
+	row = constants.ROW_NUMBERS.index(number) + 1
 
-	#Replace the old x letter with its number representative
-	localation_List[0] = row_IDX
-	
-	return localation_List
+	return [column, row]
 
-	
 #This does the opposite of the previous function: It takes an x and a y coordinate ex(1, 2)
-#And then converts it into the form that TurtleMove.placePiece can use. 
+#And then converts it into the form that turtleMove.placePiece can use.
 #Returns: a char and a integer
 #Ex ['A', 2]
 def convertMoveType(x, y):
-	row = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-	x = row[x]
-	move = []
-	move.append(x)
-	move.append(y)
-	
-	return move
-	
+	x = constants.COLUMN_LETTERS[x]
+
+	return [x, y]
+
 #Takes a the turn letter and returns the opposite turn letter
 #Ex 'W' returns 'B'
 # and 'B' returns 'W'
 def inverseTurnChar(char):
-	if char== 'W':
-		return 'B'
-	elif char == 'B':
-		return 'W'
+	if char == constants.PIECE_BLACK:
+		return constants.PIECE_WHITE
+	elif char == constants.PIECE_WHITE:
+		return constants.PIECE_BLACK
 	else:
 		return 'Error Invalid Param'
-		
+
 def charToPiece(x, y, temp_piece):
 	conv_move = convertMoveType(x, y)
-	Tx = conv_move[0]
-	Ty = conv_move[1] + 1 #Add 1 in order to start counting at 1
-	
+	temp_x = conv_move[0]
+	temp_y = conv_move[1] + 1 #Add 1 in order to start counting at 1
+
 	#Get the colour of the piece
-	if temp_piece != 'N':
-		if temp_piece == "B":
-			temp_piece = "Black"
+	if temp_piece != constants.PIECE_NONE:
+		if temp_piece == constants.PIECE_BLACK:
+			color = "Black"
 		else:
-			temp_piece = "White"
+			color = "White"
 		#Place the piece on the board in the appropriate spot
-		TurtleMove.placePiece(Tx, Ty, temp_piece)
+		turtleMove.placePiece(temp_x, temp_y, color)
 
 #IMPORTANT FUNCTION: - Any Logic Errors in the will be persvasive
 #<<<<< FUNCTION ASSUMES ALL PASSED PARAMETERS ARE ALREADY VALIDATED
@@ -83,7 +62,7 @@ def charToPiece(x, y, temp_piece):
 #Function then updates the row as if it was horizontal
 #Works for "rows" of all sizes
 #returns the updated row
-		
+
 def updateRow(row, relative_location, turn_letter):
 	#Declare all necessary variables that we need to immediately use
 	start_convert = False
@@ -92,135 +71,135 @@ def updateRow(row, relative_location, turn_letter):
 	que = []
 	stop = False
 	loop_counter = 0
-	
-	
+
+
 	#Row left has to be reveresed so that we can treat it the same way as row right.
 	row_left.reverse()
-	
+
 	#For loop finds all the valid letters in the specified row and adds them to the que
 	#To be a valid letter, the letter must be the inverse turn character,
 		#And must have a turn letter on the other side of another inverse turn letter
-	
+
 	#There are two main triggers that handle this:
 		#stop is a generic preventer that is used to prevent additional characters from being added to the que
 		#start_convert marks when to add the character to the que
 	for letter in row_left:
-		
+
 		if letter == inverseTurnChar(turn_letter) and stop == False:
 			start_convert = True
-			
-		
-		if letter == 'N':
+
+
+		if letter == constants.PIECE_NONE:
 			start_convert == False
 			stop = True
-		
+
 		if letter == turn_letter and loop_counter > 0:
 			start_convert = False
 			stop = True
 			end_found = True
-			
-			
+
+
 		#We need to check that the letter beside is not empty, or outside of the board
 		#This can be handled simply with the below try - except
 		#loop_counter tracks the index of the list
 		try:
-			if row_left[loop_counter+1] == 'N':
+			if row_left[loop_counter+1] == constants.PIECE_NONE:
 				start_convert = False
-			
-			
+
+
 		except:
 			start_convert = False
-			
-		#If the above conditions have been met we can add the letter to the que	
+
+		#If the above conditions have been met we can add the letter to the que
 		if start_convert == True and letter == inverseTurnChar(turn_letter):
 			que.append(letter)
-		
-		
+
+
 		loop_counter = loop_counter +1
-		
-	
-	#Use a different index counter 
+
+
+	#Use a different index counter
 	counter = 0
-	
-	#If the end has been found and the que actually has letters in it, 
+
+	#If the end has been found and the que actually has letters in it,
 	#then we can change the letters in the que and reinsert them back into the row
-	
+
 	if end_found == True and len(que) > 0:
 		for letter in que:
 			row_left[counter] = turn_letter
 			counter = counter+1
-	
-	#Undo the reversal that we did at the start of the code, so it can be safely added back 
+
+	#Undo the reversal that we did at the start of the code, so it can be safely added back
 	row_left.reverse()
-	
-	
+
+
 
 	#Complete the same process on the righthand side of the row.
 	#To do this we reset the variables needed
-	
+
 	start_convert = False
-	row_right = row[relative_location+1:] 	
+	row_right = row[relative_location+1:]
 	#row_right does not include the actual placed move so we need to add +1 to the relative_location
-	
+
 	end_found = False
 	que2 = []
 	stop = False
 	loop_counter = 0
-	
-	
-	
+
+
+
 	#Identicle process that used on row_left just with row_right and a different que
-	
+
 	for letter in row_right:
-		
+
 		if letter == inverseTurnChar(turn_letter) and stop == False:
 			start_convert = True
-		
-		if letter == 'N':
+
+		if letter == constants.PIECE_NONE:
 			start_convert == False
 			stop = True
 		if letter == turn_letter and loop_counter > 0:
 			start_convert = False
 			end_found = True
 			stop = True
-		
+
 		try:
-			if row_right[loop_counter+1] == 'N':
+			if row_right[loop_counter+1] == constants.PIECE_NONE:
 				start_convert = False
-			
-			
+
+
 		except:
 			start_convert = False
-		
+
 		if start_convert == True and letter == inverseTurnChar(turn_letter):
 			que2.append(letter)
-		
+
 		loop_counter = loop_counter +1
-	
-		
-		
-	
+
+
+
+
 	counter = 0
 	if end_found == True and len(que2) > 0:
 		for letter in que2:
 			row_right[counter] = turn_letter
 			counter = counter+1
-	
-	
-	
-	
-	
+
+
+
+
+
 	#newRow is what will be returned
 	#Combine left and right rows
 	new_row = row_left
 	#Add the turn letter in the location needed
 	new_row.append(turn_letter)
 	new_row = new_row + row_right
-	
-	
+
+
 	return new_row
-	
-	
+
+
 #Updates the board game_state vertically
 #Takes the game_state as a 2D list, the relative location, and the turn letter
 #Returns the updated list
@@ -230,31 +209,31 @@ def updateColumn(game_state, x, y, turn_letter):
 	temp_row = []
 	for row in game_state:
 		temp_row.append(row[x])
-	
-	
+
+
 	#Update the row
 	temp_row = updateRow(temp_row, y, turn_letter)
-	
+
 	#Using the updated row, change the game_state
 	new_game_state = game_state [:]
 	loop_count = 0
-	
+
 	#loop_count is what tracks where we are in the updated row, and acts as an index
 	for row in new_game_state:
 		if loop_count < len(temp_row):
 			#Update the piece in the location in the game_state
 			temp_piece = temp_row[loop_count]
 			row[x] = temp_piece
-			
+
 			charToPiece(x, loop_count, temp_piece)
-			
-		
-				
-			
+
+
+
+
 		loop_count = loop_count + 1
 
 	return new_game_state
-	
+
 def getDiagnal(game_state, x, y, deltaX, deltaY):
 	#Declare all necessary variables
 	edge_found = False
@@ -286,40 +265,40 @@ def getDiagnal(game_state, x, y, deltaX, deltaY):
 
 	return diag_row
 
-	
-	
+
+
 #Function updates the game state for diagnals based on the change desired, then places them on the board
 #Takes many parameters because this has to be a very versatile function
 #game_state is the 2d list representing the board
 #diag_row is the row that represents the diagnal to insert
 #x is the coord, and likewise y is the y coord (number based on a 0 index)
 #relative is the relative location in the diag_row that the piece was placed
-#deltaX is the change in x, used for handling directions. 
+#deltaX is the change in x, used for handling directions.
 #deltaY is the change in y, used to handling directions
 #deltaIteration handles if the function moves forward or backward in the string
 
-def diagCore(game_state, diag_row, x, y, relative, deltaX, deltaY, deltaIteration):	
-	
-	xTracker = x 
-	yTracker = y 
-	iteration = relative-1 
+def diagCore(game_state, diag_row, x, y, relative, deltaX, deltaY, deltaIteration):
+
+	xTracker = x
+	yTracker = y
+	iteration = relative-1
 	for letter in diag_row:
 		if 0<= yTracker <=7:
 			temp_row = game_state[yTracker]
 			if 0 <= xTracker <= 7:
 				if 0<= iteration <= len(diag_row)-1:
-					
+
 					piece = diag_row[iteration]
 					temp_row[xTracker] = piece
 					game_state[yTracker] = temp_row
-					
-					charToPiece(xTracker, yTracker, piece)	
-					
+
+					charToPiece(xTracker, yTracker, piece)
+
 		xTracker = xTracker + deltaX
 		yTracker = yTracker + deltaY
 		iteration = iteration + deltaIteration
 
-		
+
 #Function updates the board's Up to Down diagnals based on a game_state, move and turn_letter
 #Up to down is styled like this:
 		#
@@ -333,37 +312,37 @@ def diagCore(game_state, diag_row, x, y, relative, deltaX, deltaY, deltaIteratio
 	#After updating it, we can change the characters using the same way that we found the diagnals
 
 def updateDiagnalDU2(game_state, x, y, turn_letter):
-	
+
 	#Get the diagnals in both directions
 	left_row = getDiagnal(game_state, x, y, -1, 1)
 	left_row.reverse()
 	right_row = getDiagnal(game_state, x, y, 1, -1 )
-	
+
 	#Form the the row that is to updated, and add in the turn_letter
 	temp_row = left_row
 	temp_row.append(turn_letter)
 	temp_row = temp_row + right_row
-	
-	
+
+
 	#find the location of the turn_letter relative to the diagnal, which is needed to update the row
-	relative = len(left_row) 
-	
+	relative = len(left_row)
+
 
 	#Update the row
 	diag_row = updateRow(temp_row, relative-1, turn_letter)
-	
-	
+
+
 
 	#update in the NE direction
 	diagCore(game_state, diag_row, x, y, relative, 1, -1, 1)
 
 	#update in the SE direction
 	diagCore(game_state, diag_row, x, y, relative, -1, 1, -1)
-	
-	
+
+
 	return game_state
-	
-	
+
+
 
 #Function updates the board's Up to Down diagnals based on a game_state, move and turn_letter
 #Up to down is styled like this:
@@ -380,33 +359,33 @@ def updateDiagnalDU2(game_state, x, y, turn_letter):
 #Function works the same as the other diagnal updater, just with a modified dirction addition
 def update_diagnalUD2(game_state, x, y, turn_letter):
 	#Get the diagnals in both directions
-	left_row = getDiagnal(game_state, x, y, 1, 1)	
+	left_row = getDiagnal(game_state, x, y, 1, 1)
 	left_row.reverse()									#SE
 	right_row = getDiagnal(game_state, x, y, -1, -1)		#NW
-	
-	
+
+
 	#Form the the row that is to updated, and add in the turn_letter
 	temp_row = left_row
 	temp_row.append(turn_letter)
 	temp_row = temp_row + right_row
-	
-	
+
+
 	#find the location of the turn_letter relative to the diagnal, which is needed to update the row
-	relative = len(left_row) 
-	
+	relative = len(left_row)
+
 	#Update the row
 	diag_row = updateRow(temp_row, relative-1, turn_letter)
-	
+
 	#Apply the update to the old game_state
-	
+
 	#update in the NW direction
 	diagCore(game_state, diag_row, x, y, relative, -1, -1, 1)
 
 	#update in the SE direction
 	diagCore(game_state, diag_row, x, y, relative, 1, 1, -1)
-	
-	
-	
+
+
+
 	return game_state
 	#We now need to update the game_state to match the diag that was just updated
 
@@ -420,58 +399,58 @@ def updateGameState(game_state, move, turn_letter):
 	xy = convertMove(move)
 	x = xy[0]-1
 	y = xy[1]-1
-	
+
 	temp_game_state = game_state[:]
-	
+
 	#Update the column
 	temp_game_state = updateColumn(game_state, x, y, turn_letter)
-	
+
 	temp_row = updateRow(temp_game_state[y], x, turn_letter)
 	temp_game_state[y] = temp_row
 	#Update the row
-	
+
 	#Because updateRow does not draw the pieces on the board, unlike the other update functions, we must draw them here
 	counter = 0
 	for i in temp_row:
-		
+
 		conv_move = convertMoveType(counter, y+1)
 		Tx = conv_move[0]
-		Ty = conv_move[1] 
-		if i != 'N':
-				if i == "B":
+		Ty = conv_move[1]
+		if i != constants.PIECE_NONE:
+				if i == constants.PIECE_BLACK:
 					temp_piece = "Black"
 				else:
 					temp_piece = "White"
-				TurtleMove.placePiece(Tx, Ty , temp_piece)
+				turtleMove.placePiece(Tx, Ty , temp_piece)
 		counter = counter +1
-	
-	
-	
+
+
+
 	#Update the diagnals in both directions
 	temp_game_state = updateDiagnalDU2(temp_game_state, x, y, turn_letter)
 	temp_game_state = update_diagnalUD2(game_state, x, y, turn_letter)
-	
-	
+
+
 	#Ensure that the move is updated in the token
 	final_row = temp_game_state[y]
 	final_row[x] = turn_letter
 	temp_game_state[y] = final_row
-	
-	 
+
+
 	return temp_game_state
 
 #Generic test code
 #Do not worry about this, it was just used for debugging
 def testCode():
-	
+
 	#row = ['N', 'N', 'N', 'W', 'B', 'N', 'N', 'N']
 	#print(row)
 	#row = updateRow(row, 2, 'B')
 	#print(row)
 	"""
-	print("CHECK HORIZONTAL BASIC::>>>")	
+	print("CHECK HORIZONTAL BASIC::>>>")
 	print("  A    B    C    D    E    F    G    H")
-	game_state =         [['W', 'B', 'B', 'B', 'B', 'B', 'B', 'W', ]]  
+	game_state =         [['W', 'B', 'B', 'B', 'B', 'B', 'B', 'W', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
@@ -479,22 +458,22 @@ def testCode():
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
-	
+
 	for row in game_state:
 		print(row)
 	print("  ")
 	print("  is now  >>>")
 	print("  A    B    C    D    E    F    G    H")
-		
+
 	game_state = updategame_state(game_state, 'C1', 'W')
 	for row in game_state:
 		print(row)
-	
-	
+
+
 	game_state = []
 	print("CHECK VERTICAL BASIC::>>>")
 	print(" A    B    C    D    E    F    G    H")
-	game_state =         [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'W', ]]  
+	game_state =         [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'W', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'W', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'B', ]]
@@ -507,17 +486,17 @@ def testCode():
 	print("  ")
 	print("  is now  >>>")
 	print(" A    B    C    D    E    F    G    H")
-		
+
 	game_state = updategame_state(game_state, 'H2', 'W')
 	for row in game_state:
 		print(row)
-	
+
 	"""
-	
+
 	game_state = []
 	print("CHECK DU DIAGNAL BASIC::>>>")
 	print(                    "  A    B    C    D    E    F    G    H")
-	game_state =         	  [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]  
+	game_state =         	  [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'W', 'B', 'N', 'N', 'N', ]]
@@ -527,11 +506,11 @@ def testCode():
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	for row in game_state:
 		print(row)
-	
+
 	print("  ")
 	print("  is now  >>>")
 	print(" A    B    C    D    E    F    G    H")
-		
+
 	game_state = updateGameState(game_state, 'F3', 'W')
 	for row in game_state:
 		print(row)
@@ -539,7 +518,7 @@ def testCode():
 	game_state = []
 	print("CHECK UD DIAGNAL BASIC BASIC::>>>")
 	print(" A    B    C    D    E    F    G    H")
-	game_state =         [['B', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]  
+	game_state =         [['B', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'W', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'W', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
@@ -549,22 +528,22 @@ def testCode():
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'B', ]]
 	for row in game_state:
 		print(row)
-		
+
 	print("  ")
 	print("  is now  >>>")
 	print(" A    B    C    D    E    F    G    H")
-		
+
 	game_state = updategame_state(game_state, 'D4', 'B')
 	for row in game_state:
 		print(row)
-		
-	
-	
-	
+
+
+
+
 	game_state = []
 	print("CHECK UD DIAGNAL BASIC BASIC::>>>")
 	print("  A    B    C    D    E    F    G    H")
-	game_state =         [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]  
+	game_state =         [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
@@ -574,21 +553,21 @@ def testCode():
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	for row in game_state:
 		print(row)
-		
+
 	print("  ")
 	print("  is now  >>>")
 	print(" A    B    C    D    E    F    G    H")
-		
+
 	game_state = updategame_state(game_state, 'A1', 'B')
 	for row in game_state:
 		print(row)
-	
-		
+
+
 	game_state = []
 	print("CHECK CUSTOM BASIC BASIC::>>>")
 	print("  A    B    C    D    E    F    G    H")
-	
-	game_state =         [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]  
+
+	game_state =         [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'W', 'B', 'N', 'N', 'N', ]]
@@ -598,21 +577,21 @@ def testCode():
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'W', 'N', ]]
 	tempCount = 1
 	for row in game_state:
-		
+
 		print(tempCount, " ", row)
 		tempCount = tempCount +1
-	
+
 	tempCount = 1
 	print("  ")
 	print("  is now  >>>")
-	
-	
+
+
 	game_state = updategame_state(game_state, 'H8', 'B')
 	print("      A    B    C    D    E    F    G    H")
 	for row in game_state:
 		print(tempCount, " ", row)
 		tempCount = tempCount +1
-	
+
 	"""
 	""""
 	['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N']
@@ -624,7 +603,7 @@ def testCode():
 ['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N']
 ['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N']
 ['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N']
-	row = ['B', 'W', 'W', 'N', 'W', 'B', 'W', 'B']	
+	row = ['B', 'W', 'W', 'N', 'W', 'B', 'W', 'B']
 	print(row)
 	row = updateRow(row, 2, 'B')
 	print(row)
