cd9f5557e6f8698fe2bf9a5f22dab7f55cf4bdf9
inderpreet.dhillon <inderpreet.dhillon@zone29-eb.cs.ucalgary.ca>
2015-12-07 10:41:33 -0700

    Commented

--- team_tasks/task8/victoryStatus.py
+++ team_tasks/task8/victoryStatus.py
@@ -5,19 +5,30 @@ import constants
 #Params: piece, The piece to count occurences of
 #		 game_state, The string to check
 #Returns: A count of the piece in the game_state
-def countPieces(piece, game_state):#TODO Comment this method
+def countPieces(piece, game_state):
 	count = 0
+
+	#Traverse the list
 	for y in game_state:
 		for x in y:
+			#Add to the count if piece was found
 			if x == piece:
 				count += 1
+
 	return count
 
 #Checks if the game is over
 #Params: game_state, A string representation of the board
 #Returns: True if the board is full and False otherwise
 #Author: Inderpreet Dhillon
-def endGameStatus(game_state):#TODO Comment this method
+def endGameStatus(game_state):
+	#Max possible spaces that could be filled
 	limit = constants.NUM_OF_CELLS
-	count = countPieces(constants.PIECE_BLACK, game_state) + countPieces(constants.PIECE_WHITE, game_state)
-	return count == limit
+
+	#Number of spaces that are unfilled
+	empty_spaces = countPieces(constants.PIECE_NONE, game_state)
+
+	#Check if the board is full
+	board_full = (limit - empty_spaces) == limit
+
+	return board_full
