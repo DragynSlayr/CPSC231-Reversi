6c79a831c988cbc5e6c10f374a9be9df422eb174
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-12-07 23:03:15 -0700

    Commented

--- team_tasks/task8/listUpdater.py
+++ team_tasks/task8/listUpdater.py
@@ -1,30 +1,42 @@
+#This file handles all the flipping of the pieces after they are placed,
+#also heavily needed for move validation
 import turtleMove
 import constants
+import converter
 
-#Converts the location into a list of ints that represent the x y coordinates
-#Only parameter is the location as a string, ex 'A7' or 'H8'
-#Returns a list of ints: ex [1, 7] or [8, 8]
+#Converts a location to it's indexes
+#Params: location, The location to convert
+#Returs: X and Y index of the location as [X, Y]
+#Author: David Keizer
+#Editor: Inderpreet Dhillon
 def convertMove(location):
-	letter = location[0]
-	number = int(location[1])
+	#Separate column from row
+	column = location[0]
+	row = int(location[1])
 
-	column = constants.COLUMN_LETTERS.index(letter) + 1
-	row = constants.ROW_NUMBERS.index(number) + 1
+	#Get indices
+	x = constants.COLUMN_LETTERS.index(column) + 1
+	y = constants.ROW_NUMBERS.index(row) + 1
 
-	return [column, row]
+	#Return indices
+	return [x, y]
 
-#This does the opposite of the previous function: It takes an x and a y coordinate ex(1, 2)
-#And then converts it into the form that turtleMove.placePiece can use.
-#Returns: a char and a integer
-#Ex ['A', 2]
+#Inverse of convertMove, converts X and Y to a move
+#Params: x, The x of the location
+#		 y, The y of the location
+#Returns: A move of form "A1"
 def convertMoveType(x, y):
+	#Convert x
 	x = constants.COLUMN_LETTERS[x]
 
+	#Return x and y
 	return [x, y]
 
 #Takes a the turn letter and returns the opposite turn letter
-#Ex 'W' returns 'B'
-# and 'B' returns 'W'
+#Params: char, The char to inverse
+#Returns: The opposite letter to char
+#Author: David Keizer
+#Editor: None
 def inverseTurnChar(char):
 	if char == constants.PIECE_BLACK:
 		return constants.PIECE_WHITE
@@ -33,37 +45,48 @@ def inverseTurnChar(char):
 	else:
 		return 'Error Invalid Param'
 
-#Takes list based coordinates and a colour and placed the piece in the location
-#Takes x and y coordinates as well as the piece type from a list.
-#ex charToPiece(0, 0, 'B') would place a black piece at A1.
+#Places a piece at x, y of color determined by temp_piece
+#Params: x, The x coordinate to place at
+#		 y, The y coordinate to place at
+#		 temp_piece, The letter of the piece to be placed
+#Returns: None
+#Author: David Keizer
+#Editor: Inderpreet Dhillon
 def charToPiece(x, y, temp_piece):
+	#Convert x and y
 	conv_move = convertMoveType(x, y)
 	temp_x = conv_move[0]
-	temp_y = conv_move[1] + 1 #Add 1 in order to start counting at 1
 
-	#Get the colour of the piece
+	#Add 1 in order to start counting at 1
+	temp_y = conv_move[1] + 1
+
+	#Only draw in not a blank space
 	if temp_piece != constants.PIECE_NONE:
+		#Get the colour of the piece
 		if temp_piece == constants.PIECE_BLACK:
 			color = constants.PIECE_COLOR_BLACK
 		else:
 			color = constants.PIECE_COLOR_WHITE
+
 		#Place the piece on the board in the appropriate spot
 		turtleMove.placePiece(temp_x, temp_y, color)
 
-#IMPORTANT FUNCTION: - Any Logic Errors in the will be persvasive
+#IMPORTANT FUNCTION: - Any Logic Errors in the will be pervasive
 #<<<<< FUNCTION ASSUMES ALL PASSED PARAMETERS ARE ALREADY VALIDATED
 #Takes row as a list of the chars to works with, the location of the char in the string, and the letter that was placed.
 #row = 			['N', 'W', 'B', 'W', 'N', 'W', 'N']
-#Say we insert a 'B' here   ^ at the the relative location of 5
+#Say we insert a 'B' here 			  ^ at the the relative location of 5
 #Would return 	['N', 'W', 'B', 'B', 'B', 'W', 'N']
 #Function takes a specific row or list, the location that a piece has been placed, and the turn letter as parameters
 #Function then updates the row as if it was horizontal
 #Works for 'rows' of all sizes
 #returns the updated row
+#Author: David Keizer
+#Editor: None
 def updateRow(row, relative_location, turn_letter):
 	row_left = row[:relative_location]
 	row_left.reverse()
-	row_right = row[relative_location+1:]
+	row_right = row[relative_location + 1:]
 
 	row_left = updateCore(row_left, turn_letter)
 	row_left.reverse()
@@ -75,10 +98,12 @@ def updateRow(row, relative_location, turn_letter):
 
 	return new_row
 
-#Function is the core of the list updation.
+#Function is the core of the list updating
 #Takes a portion of a list in the form of a row
 #Also takes the turn_letter
 #Function returns the row updated assuming it is the right half of a list to be upadated
+#Author: David Keizer
+#Editor: None
 def updateCore(row, turn_letter):
 	#Declare Necessary Variables
 	start_que = True
@@ -86,7 +111,6 @@ def updateCore(row, turn_letter):
 	end_found = False
 
 	#Main loop adds letters to the que until the end is found
-
 	for letter in row:
 		if start_que == True:
 			if letter == inverseTurnChar(turn_letter):
@@ -99,7 +123,7 @@ def updateCore(row, turn_letter):
 				end_found = True
 
 			#Check if the end is empty, and then empty the que as it is no longer valid
-			if letter == 'N':
+			if letter == constants.PIECE_NONE:
 				end_found = False
 				start_que = False
 				que = []
@@ -116,6 +140,8 @@ def updateCore(row, turn_letter):
 #Updates the board game_state vertically
 #Takes the game_state as a 2D list, the relative location, and the turn letter
 #Returns the updated list
+#Author: David Keizer
+#Editor: None
 def updateColumn(game_state, x, y, turn_letter, draw_move = True):
 	#Create a temporary row of all of ther vertical letters for easy updating
 	temp_row = []
@@ -144,11 +170,13 @@ def updateColumn(game_state, x, y, turn_letter, draw_move = True):
 
 	return new_game_state
 
-#Function returns a diagnal based on the direction that it is given
+#Function returns a diagonal based on the direction that it is given
 #Functions takes the game state, x and y coordinates of the move
 #deltaX and deltaY represent the change in direction in the 2d list.
-#returns the diagnal that was found in said direction
-def getDiagnal(game_state, x, y, deltaX, deltaY):
+#returns the diagonal that was found in said direction
+#Author: David Keizer
+#Editor: None
+def getDiagonal(game_state, x, y, deltaX, deltaY):
 	#Declare all necessary variables
 	edge_found = False
 	yTracker = y + deltaY	#We change the x and y coord so that it skips the coordinate given
@@ -176,15 +204,17 @@ def getDiagnal(game_state, x, y, deltaX, deltaY):
 
 	return diag_row
 
-#Function updates the game state for diagnals based on the change desired, then places them on the board
+#Function updates the game state for diagonals based on the change desired, then places them on the board
 #Takes many parameters because this has to be a very versatile function
 #game_state is the 2d list representing the board
-#diag_row is the row that represents the diagnal to insert
+#diag_row is the row that represents the diagonal to insert
 #x is the coord, and likewise y is the y coord (number based on a 0 index)
 #relative is the relative location in the diag_row that the piece was placed
 #deltaX is the change in x, used for handling directions.
 #deltaY is the change in y, used to handling directions
 #deltaIteration handles if the function moves forward or backward in the string
+#Author: David Keizer
+#Editor: None
 def diagCore(game_state, diag_row, x, y, relative, deltaX, deltaY, deltaIteration, draw_move = True):
 	#Declare necessary variables
 	xTracker = x
@@ -213,25 +243,27 @@ def diagCore(game_state, diag_row, x, y, relative, deltaX, deltaY, deltaIteratio
 		yTracker = yTracker + deltaY
 		iteration = iteration + deltaIteration
 
-#Function updates the board's Up to Down diagnals based on a game_state, move and turn_letter
+#Function updates the board's Up to Down diagonals based on a game_state, move and turn_letter
 #Up to down is styled like this:
 		#
 			#
 				#
 					#
 #Function returns the updates game_state
-def updateDiagnalDU2(game_state, x, y, turn_letter, draw_move = True):
-	#Get the diagnals in both directions
-	left_row = getDiagnal(game_state, x, y, -1, 1)
+#Author: David Keizer
+#Editor: None
+def updatediagonalDU2(game_state, x, y, turn_letter, draw_move = True):
+	#Get the diagonals in both directions
+	left_row = getDiagonal(game_state, x, y, -1, 1)
 	left_row.reverse()
-	right_row = getDiagnal(game_state, x, y, 1, -1 )
+	right_row = getDiagonal(game_state, x, y, 1, -1 )
 
 	#Form the the row that is to updated, and add in the turn_letter
 	temp_row = left_row
 	temp_row.append(turn_letter)
 	temp_row = temp_row + right_row
 
-	#find the location of the turn_letter relative to the diagnal, which is needed to update the row
+	#find the location of the turn_letter relative to the diagonal, which is needed to update the row
 	relative = len(left_row)
 
 	#Update the row
@@ -245,26 +277,28 @@ def updateDiagnalDU2(game_state, x, y, turn_letter, draw_move = True):
 
 	return game_state
 
-#Function updates the board's Up to Down diagnals based on a game_state, move and turn_letter
+#Function updates the board's Up to Down diagonals based on a game_state, move and turn_letter
 #Up to down is styled like this:
 					#
 				#
 			#
 		#
 #Function returns the updates game_state
-#Function works the same as the other diagnal updater, just with a modified dirction addition
-def update_diagnalUD2(game_state, x, y, turn_letter, draw_move = True):
-	#Get the diagnals in both directions
-	left_row = getDiagnal(game_state, x, y, 1, 1)
+#Function works the same as the other diagonal updater, just with a modified dirction addition
+#Author: David Keizer
+#Editor: None
+def update_diagonalUD2(game_state, x, y, turn_letter, draw_move = True):
+	#Get the diagonals in both directions
+	left_row = getDiagonal(game_state, x, y, 1, 1)
 	left_row.reverse()									#SE
-	right_row = getDiagnal(game_state, x, y, -1, -1)		#NW
+	right_row = getDiagonal(game_state, x, y, -1, -1)		#NW
 
 	#Form the the row that is to updated, and add in the turn_letter
 	temp_row = left_row
 	temp_row.append(turn_letter)
 	temp_row = temp_row + right_row
 
-	#find the location of the turn_letter relative to the diagnal, which is needed to update the row
+	#find the location of the turn_letter relative to the diagonal, which is needed to update the row
 	relative = len(left_row)
 
 	#Update the row
@@ -279,7 +313,11 @@ def update_diagnalUD2(game_state, x, y, turn_letter, draw_move = True):
 
 	return game_state
 
-
+#Prints a game state
+#Params: game_state, The state to print
+#Returns: None
+#Author: David Keizer
+#Editor: Inderpreet Dhillon
 def showCurrentGameState(game_state):
 	print("<<<Current Game State >>>")
 	print("        A    B    C    D    E    F    G    H")
@@ -287,37 +325,33 @@ def showCurrentGameState(game_state):
 	for row in game_state:
 		print(counter , " : " , row)
 		counter = counter + 1
+
 #Function updates the game_state based on a move, that is ASSUMED VALID
 #Only updates horizontal and vertical, will update diagnol eventually
 #Takes the current game_state, the move, and the turn letter and whether update to grid
 #Returns the updated game_state
+#Author: David Keizer
+#Editor: None
 def updateGameState(game_state, move, turn_letter, draw_move = True):
-
-	#if draw_move == True:
-	#	print("<<<< Entering Update >>>>")
-	#	showCurrentGameState(game_state)
-
 	#Get the x and y coordinates in the move,
 	#Subtract 1 so they can be used as list indexes
 	xy = convertMove(move)
 	x = xy[0]-1
 	y = xy[1]-1
 
-	temp_game_state = game_state[:]
-
+	#Create a copy of the state
+	temp_game_state = converter.toList(converter.toString(game_state))
 
 	#Update the row
 	temp_row = updateRow(temp_game_state[y], x, turn_letter)
 	temp_game_state[y] = temp_row
 
-
 	#Update the column
 	temp_game_state = updateColumn(game_state, x, y, turn_letter, draw_move)
 
-	#Update the diagnals in both directions
-	temp_game_state = updateDiagnalDU2(temp_game_state, x, y, turn_letter, draw_move)
-	temp_game_state = update_diagnalUD2(game_state, x, y, turn_letter, draw_move)
-
+	#Update the diagonals in both directions
+	temp_game_state = updatediagonalDU2(temp_game_state, x, y, turn_letter, draw_move)
+	temp_game_state = update_diagonalUD2(game_state, x, y, turn_letter, draw_move)
 
 	#Update the row
 	temp_row = updateRow(temp_game_state[y], x, turn_letter)
@@ -337,18 +371,14 @@ def updateGameState(game_state, move, turn_letter, draw_move = True):
 						temp_piece = constants.PIECE_COLOR_WHITE
 					if draw_move:
 						turtleMove.placePiece(Tx, Ty , temp_piece)
-			counter = counter +1
-
-
-	#if draw_move == True:
-	#	showCurrentGameState(temp_game_state)
-	#	print("<<<< Exiting Update >>>>")
-
+			counter = counter + 1
 
 	return temp_game_state
 
 #Generic test code
 #Do not worry about this, it was just used for debugging
+#Author: David Keizer
+#Editor: None
 def testCode():
 	row = ['N', 'N', 'W', 'W', 'W', 'N', 'N', 'N']
 	print(row)
@@ -400,7 +430,7 @@ def testCode():
 	"""
 
 	game_state = []
-	print("CHECK DU DIAGNAL BASIC::>>>")
+	print("CHECK DU diagonal BASIC::>>>")
 	print(                    "  A    B    C    D    E    F    G    H")
 	game_state =         	  [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
@@ -422,7 +452,7 @@ def testCode():
 		print(row)
 	""""
 	game_state = []
-	print("CHECK UD DIAGNAL BASIC BASIC::>>>")
+	print("CHECK UD diagonal BASIC BASIC::>>>")
 	print(" A    B    C    D    E    F    G    H")
 	game_state =         [['B', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'W', 'N', 'N', 'N', 'N', 'N', 'N', ]]
@@ -444,7 +474,7 @@ def testCode():
 		print(row)
 
 	game_state = []
-	print("CHECK UD DIAGNAL BASIC BASIC::>>>")
+	print("CHECK UD diagonal BASIC BASIC::>>>")
 	print("  A    B    C    D    E    F    G    H")
 	game_state =         [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
 	game_state = game_state + [['N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', ]]
